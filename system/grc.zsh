92da2b044bf9932d4673a39f7ac07b5199311c61
msuzoagu <msuzoagu@users.noreply.github.com>
2022-04-25 17:38:09 -0400

    adds correct path for grc to colorize brew commands

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,5 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
-  source `brew --prefix`/opt/homebrew/etc/grc.bashrc
+  source `brew --prefix`/etc/grc.zsh 
 fi

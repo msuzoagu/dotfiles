0e81aeb903ce07f91773423771aced4e3cacf0d1
msuzoagu <msuzoagu@users.noreply.github.com>
2022-05-01 19:10:18 -0400

    makes subl command availabe on commandline by adding Sublime Text bin folder to PATH

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -31,4 +31,33 @@ install_homebrew() {
 	fi 
 }
 
+# Update Homebrew
+update_homebrew() {
+	echo "Updating Homebrew"
+	echo "${YELLOW}> brew update"
+	brew update
+	echo "${RESET}"
+	echo " "
+}
+
+# Install software and applications via Brewfile
+install_software_packages() {
+	echo "Installing Homebrew Packages"
+	echo "${YELLOW}> brew bundle"
+	brew bundle
+	echo "${RESET}"
+	echo " "
+}
+
+# Verify the state of rbenv installation
+run_rbenv_doc() {
+	echo "verifying the state of rbenv installation"
+	echo "${YELLOW}"
+	curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-doctor | bash
+	echo "${RESET}"
+}
+
 install_homebrew
+update_homebrew
+install_software_packages
+run_rbenv_doc
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,6 @@ alias cs='clear'
 alias up='cd ../'
 alias reload='. ~/.zshrc'
 
-# source https://stackoverflow.com/questions/25152711/subl-command-not-working-command-not-found
-alias subl="open -a /Applications/Sublime\ Text.app" 
-
 if hash colordiff; then
     alias diff=colordiff
 fi

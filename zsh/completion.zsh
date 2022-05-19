e623cb8e292db29ea9ac2c2bb0780fc17597cb4e
msuzoagu <msuzoagu@users.noreply.github.com>
2022-05-19 18:52:43 -0400

    adds cfn-lint

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# enables aws command completion by ensuring command 
+# below runs each time you open a new shell
+complete -C '/usr/local/bin/aws_completer' aws

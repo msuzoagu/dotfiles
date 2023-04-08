e2b67e5dd7141a242dd7f156f4371edbb494ae7f
msuzoagu <msuzoagu@users.noreply.github.com>
2023-04-08 16:48:22 -0400

    corrects link to download homebrew and corrects related paths

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,6 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
+## topic/*.zsh: configuration files
+############## => example: homebrew/path.zsh 
+######################### => homebrew is topic; path.zsh is config file
+###############################  => every .zsh file in a topic folder is added to fpath
+###############################  => so that they can add functions and completion scripts
 for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;

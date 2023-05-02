9112256a2fb5529b0f7b8a1fe3d38611b3b498d6
msuzoagu <msuzoagu@users.noreply.github.com>
2023-05-01 22:15:41 -0400

    adds git alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,21 @@
 alias cl='clear'
+alias hm='cd ~'
 alias up='cd ../'
 alias reload='. ~/.zshrc'
+
+alias gl="git log"
+alias gc="git clone"
+alias gb="git branch"
+alias gaa="git add ."
+alias gst="git status"
+alias gch="git checkout"
+alias gcm="git commit -m"
+alias gpo="git push origin"
+alias gnb="git checkout -b"
+alias grnb="git branch -m "
+alias gpato="git push -u origin"
+alias gpom="git push origin main"
+alias grao="git remote add origin"
 alias glista="git config --list | grep alias"
 
 if hash colordiff; then

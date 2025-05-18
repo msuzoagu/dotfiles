5b17292ccdccf9e05c6684de73165bf3a4df18f5
msuzoagu <msuzoagu@users.noreply.github.com>
2025-05-18 15:11:28 -0400

    edit comments

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,9 +1,22 @@
-# matches case insensitive for lowercase
+#-------------------------------------------------------------------------------
+# Matches case insensitive for lowercase
+#-------------------------------------------------------------------------------
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-# pasting with tabs doesn't perform completion
+
+#-------------------------------------------------------------------------------
+# Pasting with tabs doesn't perform completion
+#-------------------------------------------------------------------------------
 zstyle ':completion:*' insert-tab pending
 
-# enables aws command completion by ensuring command 
-# below runs each time you open a new shell
+
+#-------------------------------------------------------------------------------
+# Enables aws command completion by ensuring that the
+# command below runs whenever a new shell is opened 
+#-------------------------------------------------------------------------------
 complete -C '/usr/local/bin/aws_completer' aws
+
+#-------------------------------------------------------------------------------
+# Enable autocomplete for Terraform
+#-------------------------------------------------------------------------------
+complete -o nospace -C /opt/homebrew/Cellar/tfenv/3.0.0/versions/1.4.6/terraform terraform

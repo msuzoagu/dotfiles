d1fc2a2da7eb35b3fdebcb6a2b97bc9a516a39ca
msuzoagu <msuzoagu@users.noreply.github.com>
2025-05-18 14:53:10 -0400

    add comments for long-term clarity

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -1,8 +1,18 @@
-## enable chruby
+# ------------------------------------------------------------------------------
+# The `chruby.sh` script sets up core functionality of chruby. When sourced, it:
+#  	- Defines the chruby command
+#  	- Sets up environment variables ($RUBIES< $RUBY_ROOT, etc) 
+#  	- Cleans up cariables from previous active rubies
+# Without this, chruby isn't available as a shell command and switching fails
+# ------------------------------------------------------------------------------
 source $HOMEBREW_PREFIX/opt/chruby/share/chruby/chruby.sh
 
-## enable auto-switching to project-ruby 
-## via .ruby-version at root of project 
+# ------------------------------------------------------------------------------
+# Enables automatic switch to ruby version specified in .ruby-version  
+# ------------------------------------------------------------------------------
 source $HOMEBREW_PREFIX/opt/chruby/share/chruby/auto.sh
 
+# ------------------------------------------------------------------------------
+# Sets default ruby version 
+# ------------------------------------------------------------------------------
 chruby ruby-3.2.2

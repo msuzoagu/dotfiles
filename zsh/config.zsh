ccd7241d3e5a987b545a6f5f8ae114474d98ac43
msuzoagu <msuzoagu@users.noreply.github.com>
2025-05-18 15:47:33 -0400

    update comments

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,28 +9,40 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
 setopt CORRECT
-setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
-
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_VERIFY
+setopt PROMPT_SUBST
+setopt NO_LIST_BEEP
+setopt COMPLETE_IN_WORD
 setopt HIST_REDUCE_BLANKS
+setopt APPEND_HISTORY 	# adds history
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt LOCAL_TRAPS 			# allow functions to have local traps
+setopt SHARE_HISTORY 		# share history between sessions ???
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 
-# don't expand aliases _before_ completion has finished
+
+#-------------------------------------------------------------------------------
+# Do not nice background tasks
+#-------------------------------------------------------------------------------
+setopt NO_BG_NICE 
+
+#-------------------------------------------------------------------------------
+# Adds history incrementally and share it across sessions
+#-------------------------------------------------------------------------------
+setopt INC_APPEND_HISTORY SHARE_HISTORY 
+
+#-------------------------------------------------------------------------------
+# Do not expand aliases _before_ completion has finished
 #   like: git comm-[tab]
+#-------------------------------------------------------------------------------
 setopt complete_aliases
 
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line

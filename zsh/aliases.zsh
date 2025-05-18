b76f7c330da7b33e8ca29a623999822b7fbf9a79
msuzoagu <msuzoagu@users.noreply.github.com>
2025-05-18 15:13:48 -0400

    edit comment for long-term clarity

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,23 +3,38 @@ alias up="cd ../"
 alias hm="cd $HOME"
 alias reload="exec $SHELL"
 
+
+#-------------------------------------------------------------------------------
+# Rails
+#-------------------------------------------------------------------------------
+alias be="bundle exec"
+alias rake="bundle exec rake"
+alias rspec="bundle exec rspec"
+
+
+#-------------------------------------------------------------------------------
+# Git 
+#-------------------------------------------------------------------------------
+compdef _git g='git'
 alias ga="git add"
 alias gd="git diff"
 alias gl="git log"
-alias gs="git status"
 alias gb="git branch"
+alias gst="git status"
+alias gpo="git push origin"
+alias gpato="git push -u origin"
 alias gaa="git add ."
 alias gch="git checkout"
 alias gcl="git clone"
 alias gcm="git commit -m"
 alias gnb="git checkout -b"
-alias gpo="git push origin"
 alias grb="git branch -m "
-alias gpat="git push -u origin"
-alias gpom="git push origin main"
 alias grao="git remote add origin"
 alias gcan="git commit --amend --no-edit"
 
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 if hash colordiff; then
     alias diff=colordiff
 fi
